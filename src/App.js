236efa7ba48272491743ce8eb713b1b9a0e4b1b4
daniel-takacs <takacsd89@gmail.com>
2021-04-29 12:26:48 +0300

    fetching

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const url = "https://www.nasdaq.com/api/v1/historical/AAPL/stocks/2020-01-20/2021-01-20"
+    const headers = new Headers();
+    headers.append('Accept', 'application/json');
+    const request = new Request(url,
+      {
+        method: 'POST',
+        headers: {
+          'Accept-Encoding': 'deflate',
+          'Connection': 'keep-alive',
+          'User-Agent': 'Script',
+        },
+        mode: 'cors'
+      }
+    );
+    fetch(request)
+    .then(
+    response => response.json()
+    )
+  }, [])
+
+console.log(request)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      start
     </div>
   );
 }
